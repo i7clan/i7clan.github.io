4d1476af6b9ddab0746ad10b87ae401aadcd2e42
i7clan <i7clanrivals@gmail.com>
2026-08-23 14:07:50 +0500

    Add files via upload

---
+++ members.js
@@ -0,0 +1,17 @@
+const members = [
+
+    {
+        username: "lelouch",
+        role: "MEMBER",
+        status: "ACTIVE",
+        roblox: "https://www.roblox.com/users/2843371172/profile"
+    },
+
+    {
+        username: "Zyro",
+        role: "MEMBER",
+        status: "ACTIVE",
+        roblox: "https://www.roblox.com/users/11115227985/profile"
+    }
+
+];
